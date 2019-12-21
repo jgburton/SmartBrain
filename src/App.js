0803de1942540a184c0e740317e38b669d948bc8
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-12-21 23:50:17 +0000

    Set up react app and built out Navigation and Logo Components in basic form

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+import Navigation from './components/Naviagtion/Navigation';
+import Logo from './components/Logo/Logo';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+       {/*
+      <ImageLinkForm />
+      <FaceRegognition /> */}
+
+    </div>
+  );
+}
+
+export default App;
