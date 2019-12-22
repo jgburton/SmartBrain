a42585496c296ad8eef268d324d88f04893092cc
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-12-22 00:43:09 +0000

    created Rank and ImageLikForm components. Implemented Particles.js for interesting background.

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+
+          <div className="white f3 center">
+            {'Jamie, your current rank is...'}
+          </div>
+
+          <div className="white f1 center">
+            {'#5'}
+          </div>
+
+        </div>
+    )
+}
+
+export default Rank;
