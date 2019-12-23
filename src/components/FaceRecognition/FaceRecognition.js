b8e99eeef63354123f4e012311b5ac17e2ad9edb
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-12-23 00:09:20 +0000

    Setting up Clairfai API for face detection, creating basic FaceRecognition component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FaceRecognition = () => {
     return (
-       <div className="center">
+       <div className="center pa4">
            <img src={"https://samples.clarifai.com/face-det.jpg"} alt="" />
        </div>
     )
